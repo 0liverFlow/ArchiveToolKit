5e7c195e1226b94ca4f73c066084ae373209a8f6
Olivier Konaté <64969369+0liverFlow@users.noreply.github.com>
2023-04-27 10:16:01 -0700

    Update ArchiveToolKit.sh

--- ArchiveToolKit.sh
+++ ArchiveToolKit.sh
@@ -131,19 +131,19 @@ decompress_file(){
 	echo -e "${BOLDCYAN}[*] Starting decompression...${NO_COLOR}"
 	case $compression_algorithm in
 		"gzip")
-			if [[ $compression_filename == *.tar.gz ]]
+			if [[ $compressed_filename == *.tar.gz ]]
 			then
 				tar -zxf "$compressed_filename" -C $file_parent_directory
-			elif [[ $compression_filename == *.gz ]]
+			elif [[ $compressed_filename == *.gz ]]
 			then
 				gunzip $compressed_filename > "$file_parent_directory/$output_filename" 
 			fi
 			;;
 		"bzip2")
-			if [[ $compression_filename == *.tar.bz2 ]]
+			if [[ $compressed_filename == *.tar.bz2 ]]
 			then
 				tar -jxf "$compressed_filename" -C $file_parent_directory
-			elif [[ $compression_filename == *.bz2 ]]
+			elif [[ $compressed_filename == *.bz2 ]]
 			then
 				bunzip $compressed_filename > "$file_parent_directort/$output_filename"
 			fi
